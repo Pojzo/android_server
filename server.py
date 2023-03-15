9896037be87c9d62ccb205bc35485393d70c8aaa
Peter Kováč <pojzinko8@gmail.com>
2023-03-16 00:21:22 +0100

    Funguje to

--- server.py
+++ server.py
@@ -1,20 +1,22 @@
 from config import *
 import socket
 
-HOST = '127.0.0.1'
-PORT = 44444
+HOST = '192.168.100.7'
+PORT = 12345
 
 # create a TCP server socket that listens on port 44444 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
+    print("Server is listening on port 12345")
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
         while True:
-            data = conn.rect(1024)
-            if not data:
+            data = conn.recv(1024)
+            if data == "BYE":
                 break
-
+            
+            print(data.decode())
             conn.sendall(data)
